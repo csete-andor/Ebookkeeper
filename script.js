944eed8be740c4db407b570996eca095a25d0839
csete-andor <admin@cseteandor.hu>
2023-05-25 20:27:02 +0200

    update

--- script.js
+++
@@ -1,8 +0,0 @@
-    var blob = document.getElementById('blob');
-
-    // Update blob position on mousemove
-    document.addEventListener('mousemove', function(e) {
-      var x = e.clientX;
-      var y = e.clientY;
-      blob.style.transform = 'translate(' + x + 'px, ' + y + 'px)';
-    });
